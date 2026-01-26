bb2b114e4b3edad8a9a47ed49e104476280c6cd5
unalivajko926-coder <unalivajko926@gmail.com>
2026-01-26 21:23:51 +0500

    Lab 1: Created Book class with public fields and DisplayInfo method

---
+++ LibraryApp/Models/Book.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryApp.Models
+{
+    public class Book
+    {
+        public string Title;
+        public string Author;
+        public int Year;
+
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"Название: {Title}, Автор: {Author}, Год: {Year}");
+
+
+        }
+    }
+}
+
--- LibraryApp/Program.cs
+++ LibraryApp/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using LibraryApp.Models;
+
+namespace LibraryApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Book myBook = new Book();
+            myBook.Title = "1984";
+            myBook.Author = "Джордж Оруэлл";
+            myBook.Year = 1949;
+            myBook.DisplayInfo();
+        }
+    }
+}
