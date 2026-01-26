27f5e9579575c2106e54a4bea0bb7150cc8dcbc4
unalivajko926-coder <unalivajko926@gmail.com>
2026-01-26 21:30:07 +0500

    Lab 2: Implemented encapsulation, properties, constructor with validation

--- LibraryApp/Models/Book.cs
+++ LibraryApp/Models/Book.cs
@@ -8,9 +8,9 @@ namespace LibraryApp.Models
 {
     public class Book
     {
-        public string Title;
-        public string Author;
-        public int Year;
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public int Year { get; set; }
 
         public void DisplayInfo()
         {
@@ -18,6 +18,21 @@ namespace LibraryApp.Models
 
 
         }
+
+        public Book(string title, string author, int year)
+        {
+            if (string.IsNullOrWhiteSpace(title))
+                throw new ArgumentException("Название не может быть пустым", nameof(title));
+    	    if (string.IsNullOrWhiteSpace(author))
+                throw new ArgumentException("Автор не может быть пустым", nameof(author));
+            if (year< 0 || year> DateTime.Now.Year + 1)
+                throw new ArgumentOutOfRangeException(nameof(year), "Недопустимый год издания");
+            
+            Title = title;
+            Author = author;
+            Year = year;
     }
+
+}
 }
 
--- LibraryApp/Program.cs
+++ LibraryApp/Program.cs
@@ -6,11 +6,21 @@ namespace LibraryApp
     {
         static void Main(string[] args)
         {
-            Book myBook = new Book();
-            myBook.Title = "1984";
-            myBook.Author = "Джордж Оруэлл";
-            myBook.Year = 1949;
-            myBook.DisplayInfo();
+            try
+            {
+                Book book1 = new Book("1984", "Дж. Оруэлл", 1949);
+                Book book2 = new Book("Гарри Поттер", "Дж. Роулинг", 1997);
+                Book book3 = new Book("Мастер и Маргарита", "М. Булгаков", 1967);
+
+                book1.DisplayInfo();
+                book2.DisplayInfo();
+                book3.DisplayInfo();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Ошибка: {ex.Message}");
+            }
+
         }
     }
 }
